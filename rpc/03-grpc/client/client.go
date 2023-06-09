ce0dbdfd016dab91102c1924f8af8cddb7f807b0
liuxiaoyu <liuxiaoyu@ghostcloud.cn>
2023-06-09 09:45:02 +0800

    rpc: grpc learn

---
+++ rpc/03-grpc/client/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"learn-grpc/proto"
+	"log"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:9900", grpc.WithInsecure())
+	if err != nil {
+		panic("connect failed, err: " + err.Error())
+	}
+	defer conn.Close()
+	c := proto.NewGreeterClient(conn)
+	req := proto.HelloRequest{Name: "GGG"}
+	r, err := c.SayHello(context.Background(), &req)
+	if err != nil {
+		log.Fatal("call sayhello failed, err: ", err)
+	}
+	log.Println("call sayhello success, response: ", r.String())
+}
---
+++ rpc/03-grpc/server/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"learn-grpc/proto"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	proto.UnimplementedGreeterServer
+}
+
+func (s Server) SayHello(ctx context.Context, req *proto.HelloRequest) (*proto.HelloReply, error) {
+	return &proto.HelloReply{Message: "hello, " + req.Name}, nil
+}
+func main() {
+	g := grpc.NewServer()
+	proto.RegisterGreeterServer(g, &Server{})
+	lis, err := net.Listen("tcp", ":9900")
+	if err != nil {
+		panic("failed listen: " + err.Error())
+	}
+	log.Fatal(g.Serve(lis))
+}
